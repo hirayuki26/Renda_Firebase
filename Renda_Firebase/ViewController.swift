2187a40b3245dcb8a668370a6cc51691f40cd6d8
YukiHirayama <yuki.hira26a1@gmail.com>
2022-08-28 06:36:02 +0900

    Initial Commit

---
+++ Renda_Firebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Renda_Firebase
+//
+//  Created by Yuki Hirayama on 2022/08/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
